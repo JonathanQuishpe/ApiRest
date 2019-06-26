86eb2a552830be0a0e39bacdced71248b9047c85
jonathanquishpe <jonathan.quishpe01@epn.edu.ec>
2019-06-25 21:05:56 -0500

    Crud Usuarios

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Models\Usuarios;
+
+class UsuariosController extends Controller{
+    public function index(){
+        $user = Usuarios::get();
+        echo (json_encode($user)); 
+    }
+
+    public function store(Request $request){
+        $user = new Usuarios();
+        $user->names = $request->input('names');
+        $user->lastnames = $request->input('lastnames');
+        $user->email = $request->input('email');
+        $user->user = $request->input('user');
+        $user->pass = $request->input('pass');
+        $user->save();
+        echo json_encode($user);
+
+    }
+
+    public function update(Request $request, $id){
+        $user = Usuarios::find($id);
+        $user->names = $request->input('names');
+        $user->lastnames = $request->input('lastnames');
+        $user->email = $request->input('email');
+        $user->user = $request->input('user');
+        $user->pass = $request->input('pass');
+        $user->save();
+        echo json_encode($user);
+    }
+
+    public function destroy($id){
+        $movie = Usuarios::find($id);
+        $movie->delete();
+    }
+
+    public function show($id){
+        $user = Usuarios::find($id);
+        echo json_encode($user);
+    }
+}
